ff551408539fc8121ffe13f2f9a0d617ef777440
Mike C. Fletcher <mcfletch@sturm>
2009-04-23 15:27:11 -0400

    Add generation of a line_profiler result-set (currently segfaults, however)

--- tests/create_samples.py
+++ tests/create_samples.py
@@ -31,13 +31,26 @@ def a( count=5 ):
 if __name__ == "__main__":
     import pprint 
     command = '''x()'''
-    profiler = hotshot.Profile( "hotshot.profile", lineevents=0 )
+    profiler = hotshot.Profile( "hotshot.profile", lineevents=1 )
     profiler.runctx( command, globals(), locals())
     print dir(profiler)
     profiler.close()
+    import pdb
+    pdb.set_trace()
+    print 'hotshot line events' ,profiler.lineevents
     
     profiler = cProfile.Profile( subcalls=True )
     profiler.runctx( command, globals(), locals())
     stats = profiler.getstats()
     profiler.dump_stats( 'cprofile.profile' )
+    
+    try:
+        import line_profiler
+    except ImportError, err:
+        pass 
+    else:
+        profiler  = line_profiler.LineProfiler()
+        profiler.runctx( command, globals(), locals() )
+        profiler.dump_stats( 'line_profiler.profile' )
+    
     
