eaf1bc43ca70dc2962fa386f7cf5df70f323899e
Mike C. Fletcher <mcfletch@vrplumber.com>
2010-02-24 11:11:55 -0500

    Update docs and setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """Installs RunSnakeRun using distutils
 
 Run:
-	python setup.py install
+    python setup.py install
 to install the package from the source archive.
 """
 try:
@@ -17,45 +17,47 @@ version = [
 ][0]
 
 if __name__ == "__main__":
-	extraArguments = {
-		'classifiers': [
-			"""License :: OSI Approved :: BSD License""",
-			"""Programming Language :: Python""",
-			"""Topic :: Software Development :: Libraries :: Python Modules""",
-			"""Intended Audience :: Developers""",
-		],
-		'keywords': 'profile,gui,wxPython,squaremap',
-		'long_description' : """GUI Viewer for Python profiling runs
+    extraArguments = {
+        'classifiers': [
+            """License :: OSI Approved :: BSD License""",
+            """Programming Language :: Python""",
+            """Topic :: Software Development :: Libraries :: Python Modules""",
+            """Intended Audience :: Developers""",
+        ],
+        'keywords': 'profile,gui,wxPython,squaremap',
+        'long_description' : """GUI Viewer for Python profiling runs
 
 Provides explorability and overall visualization of the call tree
 and package/module structures.""",
-		'platforms': ['Any'],
-	}
-	### Now the actual set up call
-	setup (
-		name = "RunSnakeRun",
-		version = version,
-		url = "http://www.vrplumber.com/programming/runsnakerun/",
-		download_url = "http://www.vrplumber.com/programming/runsnakerun/",
-		description = "GUI Viewer for Python profiling runs",
-		author = "Mike C. Fletcher",
-		author_email = "mcfletch@vrplumber.com",
-		license = "BSD",
-		package_dir = {
-			'runsnakerun':'.',
-		},
-		packages = [
-			'runsnakerun',
-		],
-		options = {
-			'sdist':{'force_manifest':1,'formats':['gztar','zip'],},
-		},
-		zip_safe=False,
-		entry_points = {
-			'gui_scripts': [
-				'runsnake=runsnakerun.runsnake:main',
-			],
-		},
-		**extraArguments
-	)
+        'platforms': ['Any'],
+    }
+    ### Now the actual set up call
+    setup (
+        name = "RunSnakeRun",
+        version = version,
+        url = "http://www.vrplumber.com/programming/runsnakerun/",
+        download_url = "http://www.vrplumber.com/programming/runsnakerun/",
+        description = "GUI Viewer for Python profiling runs",
+        author = "Mike C. Fletcher",
+        author_email = "mcfletch@vrplumber.com",
+        license = "BSD",
+        package_dir = {
+            'runsnakerun':'.',
+        },
+        packages = [
+            'runsnakerun',
+        ],
+        options = {
+            'sdist':{
+                'force_manifest':1,
+                'formats':['gztar','zip'],},
+        },
+        zip_safe=False,
+        entry_points = {
+            'gui_scripts': [
+                'runsnake=runsnakerun.runsnake:main',
+            ],
+        },
+        **extraArguments
+    )
 
