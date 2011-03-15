1c17b46c207f9e73f7fd1fca0f9cce8cde0f0129
Mike C. Fletcher <mcfletch@vrplumber.com>
2011-03-14 23:50:50 -0400

    Add a test memory dump and a script to create a fairly generic one

---
+++ tests/creatememdump.py
@@ -0,0 +1,8 @@
+import sys,httplib
+
+from meliae import scanner
+filename = 'dump.memory'
+fh = open( filename, 'wb' )
+scanner.dump_all_objects( fh )
+fh.close()
+print 'saved memory dump to: %r'%( filename, )
